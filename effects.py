b8925fd1dd8b4c915163da4f0221567dc3a88b46
Garrett Ransom <g2ransom@gmail.com>
2020-03-24 22:00:05 -0700

    added logic related to EQSignal object to take in signals, calculate eq params, and use pyo audio engine to eq signals and convert back into numpy signal arrays [In Progress]

--- effects.py
+++ effects.py
@@ -1,3 +1,5 @@
+from pyo import *
+
 class Signal:
     def __init__(self, path, signal, n_fft, window_size, hop_length, R):
         self.path = path
@@ -16,14 +18,19 @@ class Signal:
         self.freqs = np.array([i * self.sr / self.fft.shape[0] for i in range(self.num_bins)])
 
 
-class EQ_signal(Signal):
-    def __init__(self, path, signal, n_fft, window_size, hop_length, R, bins, roll_percent, seconds):
+class EQSignal(Signal):
+    def __init__(self, path, signal, n_fft, window_size, hop_length, R, bins, roll_percent, seconds,
+                rank_threshold, max_n, min_overlap_ratio, max_eq):
         super().__init__(path, signal, n_fft, window_size, hop_length, R)
         self.bins = bins
         self.roll_percent = roll_percent
         self.chunk_fft_db = compute_chunk(self.norm_fft_db, self.window_size, self.sr, self.seconds)
         self.rank = compute_rank(self.chunk_fft_db)
         self.sparse_vec = compute_sparsity(self.rank, self.num_bins)
+        self.rank_threshold = rank_threshold
+        self.max_n = max_n,
+        self.min_overlap_ratio = min_overlap_ratio
+        self.max_eq = max_eq
 
     def compute_energy_percent(self):
         total_energy = np.sum(self.chunk_fft)
@@ -43,23 +50,69 @@ class EQ_signal(Signal):
         r_avg = np.mean(r_active)
         return r_avg
 
-    def overlap(self, sv1):
-        overlap_vec = self.sparse_vec * sv1
-        num_overlaps = np.sum(overlap_vec)
-        overlap_ratio = num_overlaps / overlap_vec.shape[1]
-        return overlap_vec, num_overlaps, overlap_ratio
+    def compute_mask(self, signal, overlap_vec, num_overlaps):
+        soa_vec_i = sparse_overlap_avg(self.num_bins, self.chunk_fft_db, self.sparse_vec, overlap_vec, num_overlaps)
+        soa_vec_j = sparse_overlap_avg(signal.num_bins, signal.chunk_fft_db, signal.sparse_vec, overlap_vec, num_overlaps)
+        r_soa_vec_i = rank_soa_vec(soa_vec_i)
+        r_soa_vec_j = rank_soa_vec(soa_vec_j)
+        masker_vec_i = masker_rank_vec(r_soa_vec_i)
+        maskee_vec_j = maskee_rank_vec(r_soa_vec_j)
+        mask_ij = ((masker_vec_i * maskee_vec_j) * (soa_vec_i - soa_vec_j)).flatten()
+        m_f = np.concatenate((mask_ij[:, np.newaxis], self.freqs[:, np.newaxis]), axis=1)
+        return mask_ij      
 
-    def sparse_overlap_avg(self, overlap_vec, num_overlaps):
-        soa_vec = np.zeros((self.freq_bins, 1))
-        for i in range(self.freq_bins):
-            soa_vec[i] = np.sum((self.chunk_fft[i] * self.sparse_vec) * overlap_vec) / num_overlaps
-        return soa_vec
+    def eq_params(self, signals):
+        num_signals = len(signals)
+        for i in range(num_signals):
+            mask = np.empty(shape=[0, 2])
+            eq_info = []
+            overlap_vec, num_overlaps, overlap_ratio = overlap(self.sparse_vec, signals[i].sparse_vec)
+            if (overlap_ratio > min_overlap_ratio) and (i != j):
+                mask_ij = self.compute_mask(signals[i], overlap_vec, num_overlaps)
+                m_f = np.concatenate((mask_ij[:, np.newaxis], signals[i].freqs[:, np.newaxis]), axis=1)
+                mask = np.append(mask, m_f, axis=0)
+            else:
+                mask_ij = 0
+        mask_m = np.zeros(num_bins)
+        for b in range(num_bins):
+            arr = mask[b::num_bins, :]
+            max_b, _ = np.max(arr, axis=0)
+            mask_m[b] = max_b
+        top_m = np.argsort(mask_m)[-max_n:]
+        top_m_max = mask_m[top_m].max()
+        idx = np.unravel_index(top_m, mask_m.shape)[0]
+        for x in idx:
+            freq_bin = x  * (sr / num_bins)
+            mask_val = mask_m[x]
+            if (mask_val) > 0 and (freq_bin <= 20000) and (freq_bin >= 20):
+                mask_val_scaled = (mask_val / top_m_max) * max_eq
+                eq_type = 0
+                eq_info.append([freq_bin, mask_val_scaled, eq_type])
+        rolloff = self.compute_rolloff()
+        energy_percent = self.compute_energy_percent()
+        eq_info.append([rolloff, 0.71, 2])
+        if energy_percent is not None:
+            eq_info.append([energy_percent, 0.71, 1])
+        # params_list.append({signals[i].path: eq_info})
+        return eq_info
 
-    def rank_soa_vec(self, soa_vec): return np.abs(rankdata(soa_vec, method='min') - (soa_vec.shape[0])) + 1
+    def equalization(self, eq_info, Q):
+        num_filters = len(eq_info)
+        s = Server.boot()
+        c = Converter(self.signal)
+        out = c.numpy_to_pyo(s)
+        for i in range(num_filters):
+            freq = float(eq_info[i][0])
+            gain = float(eq_info[i][1])
+            eq_type = int(eq_info[i][2])
+            out = EQ(out, freq=freq, q=Q, boost=-gain, type=eq_type)
+        out = out.out()
+        numpy_out = c.pyo_to_numpy(out, s)
+        return numpy_out
 
-    def masker_rank_vec(self, r_soa_vec): return np.expand_dims(np.where(r_soa_vec > 10, 1, 0), axis=1)
 
-    def maskee_rank_vec(self, r_soa_vec): return np.expand_dims(np.where(r_soa_vec <= 10, 1, 0), axis=1)
+class CompressSignal(Signal):
+    pass
 
 
 class Converter:
@@ -67,19 +120,15 @@ class Converter:
         self.signal = signal
         self.buffer_size = signal.shape[0]
 
-    def numpy_to_pyo(self):
-        s = Server.boot()
+    def numpy_to_pyo(self, s):
         s.start()
         t = DataTable(size=self.buffer_size)
         osc = TableRead(t, freq=t.getRate(), loop=True, mul=0.1).out()
         arr = np.asarray(t.getBuffer())
-        pyo_y = process(arr, self.signal, osc)
-        s.shutdown()
+        pyo_x = process(arr, self.signal, osc)
         return pyo_x
 
-    def pyo_to_numpy(self, out):
-        s = Server.boot()
-        s.start()
+    def pyo_to_numpy(self, out, s):
         t = DataTable(size=self.buffer_size)
         b = TableRec(out, t, 0.01).play()
         tf = TrigFunc(b["trig"], function=done, arg=t)
@@ -127,11 +176,27 @@ def compute_sparsity(rank, num_bins):
             sparse_vec[0, i] = 1
     return sparse_vec
 
+def overlap(sv0, sv1):
+    overlap_vec = sv0 * sv1
+    num_overlaps = np.sum(overlap_vec)
+    overlap_ratio = num_overlaps / overlap_vec.shape[1]
+    return overlap_vec, num_overlaps, overlap_ratio
+
+def sparse_overlap_avg(num_bins, chunk_fft_db, sparse_vec, overlap_vec, num_overlaps):
+    soa_vec = np.zeros((num_bins, 1))
+    for i in range(num_bins):
+        soa_vec[i] = np.sum((chunk_fft_db[i] * sparse_vec) * overlap_vec) / num_overlaps
+    return soa_vec
+
+def rank_soa_vec(soa_vec): return np.abs(rankdata(soa_vec, method='min') - (soa_vec.shape[0])) + 1
+
+def masker_rank_vec(r_soa_vec): return np.expand_dims(np.where(r_soa_vec > 10, 1, 0), axis=1)
+
+def maskee_rank_vec(r_soa_vec): return np.expand_dims(np.where(r_soa_vec <= 10, 1, 0), axis=1)
+
 def process(arr, x, osc):
     "Fill the array (so the table) with white noise."
     arr[:] = x
-    #do processing logic here
-    # out = EQ(osc).out()
     return osc
 
 def done(t):
